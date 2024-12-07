61331b6bfda9d22a233a16b80c8aee7094627930
Husnain Izhar <husnainizhar@outlook.com>
2024-12-07 17:15:03 +0000

    latest

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,13 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your own AMI ID
+  ami           = "ami-053b12d3152c0cc71"  # Replace with your correct AMI ID
   instance_type = "t2.micro"
-  key_name      = "your-key-name"
+  key_name      = "test"  # Ensure this key pair exists in your region
+
   tags = {
     Name = "example-instance"
   }
-}
+
