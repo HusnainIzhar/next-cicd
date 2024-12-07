6e39a06e45a7ba374ebad80609270b71fe9484ca
Husnain Izhar <husnainizhar@outlook.com>
2024-12-07 17:47:58 +0000

    latest

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,11 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "my_instance" {
   ami           = "ami-053b12d3152c0cc71"  # Replace with your correct AMI ID
   instance_type = "t2.micro"
-  key_name      = "test"  # Ensure this key pair exists in your region
 
   tags = {
-    Name = "example-instance"
+    Name = "MYEC2"
   }
 }
