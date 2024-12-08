d1a55109d9cdda5dc9c3b437ac65d6dc2df16af6
Husnain Izhar <husnainizhar@outlook.com>
2024-12-08 06:10:09 +0000

    latest

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,43 @@
-provider "aws" {
-  region = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.2"
+    }
+  }
+  
+  backend "s3" {
+    bucket = "demo-bucket-9419a72476f569ef"
+    key    = "backend.tfstate"
+    region = "eu-north-1"
+  }
 }
 
-resource "aws_instance" "my_instance" {
-  ami           = "ami-053b12d3152c0cc71"  # Replace with your correct AMI ID
-  instance_type = "t2.micro"
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "random_id" "rand_id" {
+  byte_length = 8
+}
+
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "app-bucket-${random_id.rand_id.hex}"
+}
+
+resource "aws_instance" "my_server" {
+  ami           = "ami-0c0e147c706360bd7"
+  instance_type = "t3.nano"
 
   tags = {
-    Name = "MYEC2"
+    Name = "SampleServer"
   }
+}
 
-  lifecycle {
-    prevent_destroy = true  # Prevent accidental deletion of the instance
-  }
+output "random_id_value" {
+  value = random_id.rand_id.hex
 }
