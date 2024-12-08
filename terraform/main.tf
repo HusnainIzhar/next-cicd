5dc58d168b5bb290b2a5e43e15df20484c02b04c
Husnain Izhar <husnainizhar@outlook.com>
2024-12-08 06:15:17 +0000

    latest

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.54.1"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.6.2"
-    }
   }
-  
   backend "s3" {
     bucket = "demo-bucket-9419a72476f569ef"
     key    = "backend.tfstate"
@@ -21,15 +16,8 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "random_id" "rand_id" {
-  byte_length = 8
-}
 
-resource "aws_s3_bucket" "app_bucket" {
-  bucket = "app-bucket-${random_id.rand_id.hex}"
-}
-
-resource "aws_instance" "my_server" {
+resource "aws_instance" "myserver" {
   ami           = "ami-0c0e147c706360bd7"
   instance_type = "t3.nano"
 
@@ -38,6 +26,3 @@ resource "aws_instance" "my_server" {
   }
 }
 
-output "random_id_value" {
-  value = random_id.rand_id.hex
-}
