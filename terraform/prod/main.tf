aeb01fd625dd9f87851cec340e03baf1748fe817
Husnain Izhar <husnainizhar@outlook.com>
2024-12-09 11:18:28 +0000

    latest

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -26,7 +26,6 @@ resource "aws_instance" "MyServer" {
   ami           = "ami-053b12d3152c0cc71"
   instance_type = "t2.micro"
   key_name      = "secret"
-  security_groups = ["ServerGroup"]
 }
 output "ec2_public_dns" {
   value = aws_instance.MyServer.public_dns
