81ee8e038e76459b18a2dd6557e49e022c72c47e
Husnain Izhar <husnainizhar@outlook.com>
2024-12-09 06:44:30 +0000

    latest

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -17,11 +17,40 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# Define the security group
+resource "aws_security_group" "MyServerSG" {
+  name        = "MyServerSecurityGroup"
+  description = "Allow SSH access"
 
+  # Ingress rule for SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Replace with your IP for better security
+  }
+
+  # Egress rule to allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "MyServerSecurityGroup"
+  }
+}
+
+# Define the EC2 instance
 resource "aws_instance" "MyServer" {
   ami           = "ami-053b12d3152c0cc71"
   instance_type = "t2.micro"
-   key_name      = "secret"
+  key_name      = "secret"
+
+  # Associate the security group with the instance
+  vpc_security_group_ids = [aws_security_group.MyServerSG.id]
 
   tags = {
     Name = "server1"
