f36b918cfd2db006b3d8418873b3ba1f59c37d4d
Husnain Izhar <husnainizhar@outlook.com>
2024-12-09 06:15:19 +0000

    latest

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -21,7 +21,7 @@ provider "aws" {
 resource "aws_instance" "MyServer" {
   ami           = "ami-053b12d3152c0cc71"
   instance_type = "t2.micro"
-   key_name      = "secret"
+   key_name      = "secret1"
 
   tags = {
     Name = "server"
