2632d81a8cf2e30021946c0e06144d6c089a2a81
Husnain Izhar <husnainizhar@outlook.com>
2024-12-11 14:10:28 +0000

    latest

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -28,6 +28,49 @@ resource "aws_vpc" "myvpc" {
   }
 }
 
+# Request an ACM Certificate
+resource "aws_acm_certificate" "mycert_acm" {
+  domain_name               = "ec2.${my-alb-1263533236.ap-south-1.elb.amazonaws.com}"
+  subject_alternative_names = ["*.ec2.${my-alb-1263533236.ap-south-1.elb.amazonaws.com}"]
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Create Route 53 DNS Record for Validation
+data "aws_route53_zone" "selected_zone" {
+  name         = "my-alb-1263533236.ap-south-1.elb.amazonaws.com"
+  private_zone = false
+}
+
+resource "aws_route53_record" "cert_validation_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.mycert_acm.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.selected_zone.zone_id
+}
+
+# Handle Certificate Validation
+resource "aws_acm_certificate_validation" "cert_validation" {
+  timeouts {
+    create = "5m"
+  }
+  certificate_arn         = aws_acm_certificate.mycert_acm.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
+}
+
 # Private Subnet in ap-south-1a
 resource "aws_subnet" "private_subnet_ap_south_1a" {
   cidr_block        = "10.0.1.0/24"
