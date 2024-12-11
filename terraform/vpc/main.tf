a88bb912ec07001b31e60440573405a51f131312
Husnain Izhar <husnainizhar@outlook.com>
2024-12-11 14:44:27 +0000

    latest

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -28,62 +28,6 @@ resource "aws_vpc" "myvpc" {
   }
 }
 
-# Application Load Balancer (ALB)
-resource "aws_lb" "my_alb" {
-  name               = "my-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [aws_subnet.public_subnet_ap_south_1a.id, aws_subnet.public_subnet_ap_south_1b.id]
-  enable_deletion_protection = false
-  tags = {
-    Name = "my-alb"
-  }
-}
-
-# Request an ACM Certificate
-resource "aws_acm_certificate" "mycert_acm" {
-  domain_name               = aws_lb.my_alb.dns_name
-  subject_alternative_names = ["*.${aws_lb.my_alb.dns_name}"]
-  validation_method         = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Create Route 53 DNS Record for Validation
-data "aws_route53_zone" "selected_zone" {
-  name         = "mydomain.com"  # Replace with your actual domain name
-  private_zone = false
-}
-
-resource "aws_route53_record" "cert_validation_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.mycert_acm.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.selected_zone.zone_id
-}
-
-# Handle Certificate Validation
-resource "aws_acm_certificate_validation" "cert_validation" {
-  timeouts {
-    create = "5m"
-  }
-  certificate_arn         = aws_acm_certificate.mycert_acm.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
-}
-
 # Private Subnet in ap-south-1a
 resource "aws_subnet" "private_subnet_ap_south_1a" {
   cidr_block        = "10.0.1.0/24"
@@ -309,6 +253,19 @@ resource "aws_autoscaling_group" "ec2_asg" {
     key                 = "Name"
     value               = "ec2-instance"
     propagate_at_launch = true
+  }
+}
+
+# Application Load Balancer (ALB)
+resource "aws_lb" "my_alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [aws_subnet.public_subnet_ap_south_1a.id, aws_subnet.public_subnet_ap_south_1b.id]
+  enable_deletion_protection = false
+  tags = {
+    Name = "my-alb"
   }
 }
 
