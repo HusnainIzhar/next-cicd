df2eff597049c17b5009892251a10138a33b6db2
Husnain Izhar <husnainizhar@outlook.com>
2024-12-14 08:16:32 +0000

    fds

--- terraform/final copy/modules/ec2/main.tf
+++ terraform/final copy/modules/ec2/main.tf
@@ -13,8 +13,80 @@ resource "aws_launch_template" "ec2_template" {
     associate_public_ip_address = false
   }
 
-  user_data = base64encode(templatefile("${path.module}/user-data.tpl",{project_name = var.project_name}))
+  user_data = base64encode(<<-EOF
+#!/bin/bash
 
+# Update the package manager
+sudo apt-get update -y
+
+# Install Node.js
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Install Nginx
+sudo apt-get install -y nginx
+
+# Variables
+SECRET_NAME="token"
+REPO_URL="https://github.com/HusnainIzhar/next-cicd.git"
+APP_DIR="/home/ubuntu/app"
+
+# Fetch the GitHub token from AWS Secrets Manager
+GITHUB_TOKEN=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --query SecretString --output text | jq -r '.token')
+
+# Ensure the app directory exists
+mkdir -p $APP_DIR
+cd /home/ubuntu/app/next-cicd/my-app
+
+# Clone the repository
+if [ -d "next-cicd" ]; then
+  echo "Repository already exists. Pulling latest changes..."
+  cd next-cicd
+  git reset --hard
+  git pull origin main
+else
+  echo "Cloning the repository..."
+  git clone https://$GITHUB_TOKEN@github.com/HusnainIzhar/next-cicd.git
+  cd next-cicd
+fi
+
+# Navigate to the app directory
+cd my-app
+
+# Install dependencies
+npm install
+
+# Build the app
+npm run build
+
+# Install PM2 globally
+sudo npm install -g pm2
+
+# Restart the app with PM2
+pm2 delete all
+pm2 start npm --name "nextjs-app" -- start
+
+# Configure Nginx
+sudo bash -c 'cat > /etc/nginx/sites-available/default <<EOF
+server {
+    listen 80;
+    server_name _;
+
+    location / {
+        proxy_pass http://localhost:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+}
+EOF'
+
+# Restart Nginx to apply the configuration
+sudo systemctl restart nginx
+            EOF
+  )
   tags = {
     Name = "${var.project_name}-ec2-instance"
   }
--- terraform/final copy/modules/sg/main.tf
+++ terraform/final copy/modules/sg/main.tf
@@ -38,6 +38,19 @@ resource "aws_security_group" "ec2_sg" {
     security_groups = [aws_security_group.alb_sg.id]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.alb_sg.id]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
