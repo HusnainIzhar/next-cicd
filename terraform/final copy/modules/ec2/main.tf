ed6031fdbaf81a094b0cb1a00a4ed89bc20a7e14
Husnain Izhar <husnainizhar@outlook.com>
2024-12-14 07:39:03 +0000

    lfjdslk

--- terraform/final copy/modules/ec2/main.tf
+++ terraform/final copy/modules/ec2/main.tf
@@ -13,30 +13,7 @@ resource "aws_launch_template" "ec2_template" {
     associate_public_ip_address = false
   }
 
-  user_data = base64encode(<<-EOF
-              #!/bin/bash
-              # Update the package list
-              sudo apt update -y
-              # Install Apache
-              sudo apt install -y apache2
-              # Start the Apache service
-              sudo systemctl start apache2
-              # Enable Apache to start on boot
-              sudo systemctl enable apache2
-              # Get the hostname of the EC2 instance
-              HOSTNAME=$(hostname)
-              # Create an HTML file that displays the instance's hostname
-              echo "<html>
-              <head>
-                  <title>EC2 Instance</title>
-              </head>
-              <body>
-                  <h1>Welcome to your EC2 Instance!</h1>
-                  <p>Your hostname is: \$$HOSTNAME</p>
-              </body>
-              </html>" | sudo tee /var/www/html/index.html > /dev/null
-            EOF
-  )
+  user_data = filebase64("${path.module}/user-data.sh")
 
   tags = {
     Name = "${var.project_name}-ec2-instance"
