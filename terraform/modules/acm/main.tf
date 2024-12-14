ddf32b0d1e453b33578a324847318ca3e0b90366
husnainizhar <husnainizhar@outlook.com>
2024-12-14 18:20:39 +0500

    latest

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -1,40 +1,40 @@
-resource "aws_acm_certificate" "couro_acm" {
-  domain_name               = "ec2.${var.domain_name}"
-  subject_alternative_names = ["*.ec2.${var.domain_name}"]
+# resource "aws_acm_certificate" "couro_acm" {
+#   domain_name               = "ec2.${var.domain_name}"
+#   subject_alternative_names = ["*.ec2.${var.domain_name}"]
 
-  validation_method = "DNS"
+#   validation_method = "DNS"
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-data "aws_route53_zone" "selected_zone" {
-  name         = var.domain_name
-  private_zone = false
-}
+# data "aws_route53_zone" "selected_zone" {
+#   name         = var.domain_name
+#   private_zone = false
+# }
 
-resource "aws_route53_record" "cert_validation_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.couro_acm.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+# resource "aws_route53_record" "cert_validation_record" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.couro_acm.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.selected_zone.zone_id
-}
+#   allow_overwrite = true
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 60
+#   type            = each.value.type
+#   zone_id         = data.aws_route53_zone.selected_zone.zone_id
+# }
 
-resource "aws_acm_certificate_validation" "cert_validation" {
-  timeouts {
-    create = "5m"
-  }
-  certificate_arn         = aws_acm_certificate.couro_acm.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
-}
+# resource "aws_acm_certificate_validation" "cert_validation" {
+#   timeouts {
+#     create = "5m"
+#   }
+#   certificate_arn         = aws_acm_certificate.couro_acm.arn
+#   validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
+# }
