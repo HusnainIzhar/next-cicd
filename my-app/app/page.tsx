f78a1c06aa61a7e306f203831697496052930369
husnainizhar <husnainizhar@outlook.com>
2024-12-14 18:29:31 +0500

    latest

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            This Page is Deployed on VPC codecubics{" "}
+          <li className="mb-2 text-blue-400">
+            This is Deployment{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
